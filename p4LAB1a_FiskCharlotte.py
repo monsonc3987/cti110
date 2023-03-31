574caf2dc47bad816dd95b7bdf6e6c8fccd5b22b
monsonc3987 <124844201+monsonc3987@users.noreply.github.com>
2023-03-30 23:04:28 -0400

    Add files via upload

---
+++ p4LAB1a_FiskCharlotte.py
@@ -0,0 +1,22 @@
+import turtle
+ 
+board = turtle.Turtle()
+ 
+board.forward(100)
+ 
+board.left(90)
+board.forward(100)
+ 
+board.left(139)
+board.forward(142)
+ttl = turtle.Turtle()  
+ttl.forward(150) 
+ttl.left(90)   
+ttl.forward(150)  
+ttl.left(90)  
+ttl.forward(150)  
+ttl.left(90)  
+ttl.forward(150)  
+ttl.left(90)  
+ 
+turtle.done()

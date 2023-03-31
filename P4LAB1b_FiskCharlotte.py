783abbe3f649cbad01ff977047dfd0f40bc70258
monsonc3987 <124844201+monsonc3987@users.noreply.github.com>
2023-03-30 23:02:43 -0400

    Add files via upload

---
+++ P4LAB1b_FiskCharlotte.py
@@ -0,0 +1,25 @@
+import turtle as t
+
+
+
+t.pensize(65)
+t.color("cyan")
+
+t.circle(95,-188)
+t.penup()
+t.bk(200)
+
+t.pendown()
+t.forward(100)
+t.backward(100)
+t.right(90)
+t.forward(100)
+
+t.left(90)
+t.forward(100)
+t.backward(100)
+
+t.right(90)
+t.forward(100)
+
+
